c2d5da2742769ecb4904b67a364c2c5e265c5022
mayukh551 <may14ukh@gmail.com>
2023-01-07 14:55:11 +0530

    wip

--- src/Components/Expense Filter/ExpenseFilter.tsx
+++ src/Components/Expense Filter/ExpenseFilter.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 import "./ExpenseFilter.css";
 
 const ExpensesFilter: React.FC<{
     updateSelectedYear: (year: string | null) => void;
 }> = (props) => {
-    const selectEventHandler = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectEventHandler = (event: React.ChangeEventHandler<HTMLSelectElement>) => {
         console.log(event.target.value);
         props.updateSelectedYear(event.target.value);
     };
@@ -31,6 +34,34 @@ const ExpensesFilter: React.FC<{
                             <option value={`${year}`}> {year}</option>
                         )
                     })}
+                </select>
+                {/* <Select
+                    labelId="demo-simple-select-autowidth-label"
+                    id="demo-simple-select-autowidth"
+                    value='All'
+                    sx={{backgroundColor: 'white'}}
+                    onChange={selectEventHandler}
+                    autoWidth
+                    label="Year"
+                >
+                    <MenuItem value="All">
+                        <em>All</em>
+                    </MenuItem>
+                    {yearList.map((year) => {
+                        return (
+                            <MenuItem value={`${year}`}> {year}</MenuItem>
+                        )
+                    })}
+                </Select> */}
+            </div>
+            <div className="expenses-filter__control">
+                <label>Sort By</label>
+                <select
+                    className="dropdown-sort"
+                // onChange={selectEventHandler}
+                >
+                    <option value="High to Low">High to Low</option>
+                    <option value="Low to High">Low to High</option>
                 </select>
             </div>
         </div >
