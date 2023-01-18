089883559d90f6aaca7336fe79935cea19b1ec11
mayukh551 <may14ukh@gmail.com>
2023-01-18 15:41:16 +0530

    Year wise chart generation enabled

--- src/Components/Profile/Chart/YearExpenseChart.tsx
+++ src/Components/Profile/Chart/YearExpenseChart.tsx
@@ -14,6 +14,8 @@ import {
 import { Line } from "react-chartjs-2";
 import ListContext from '../../Store/context';
 import { itemDS, ExpenseContextObj } from '../../../Models/Interfaces';
+import SelectBtn from '../../UI/SelectBtn';
+import { SelectChangeEvent } from '@mui/material/Select';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
@@ -30,8 +32,15 @@ const YearExpenseChart: React.FC = () => {
 
     const expenseList: ExpenseContextObj = useContext(ListContext);
     const [expenseData, setExpenseData] = useState<number[]>();
+    const [chartYear, setChartYear] = useState<string>('2023');
     const year: string = String(new Date().getFullYear());
     const labels = expenseList.month;
+    var yearList: string[] = [];
+
+    const currentYear = new Date().getFullYear();
+    for (let year = currentYear - 1; year >= 2019; year--) {
+        yearList.push(String(year));
+    }
 
     const data = {
         labels,
@@ -45,6 +54,10 @@ const YearExpenseChart: React.FC = () => {
         ]
     };
 
+    const selectEventHandler = (event: SelectChangeEvent<string>) => {
+        setChartYear(event.target.value);
+    };
+
     useEffect(() => {
         var expenses: number[] = [];
         if (localStorage.getItem("expenses")) {
@@ -53,7 +66,7 @@ const YearExpenseChart: React.FC = () => {
                 // updating the localStorage
                 expenses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
                 expenseList.list.forEach((item: itemDS) => {
-                    if (item.date.slice(0, 4) === year) {
+                    if (item.date.slice(0, 4) === chartYear) {
                         // console.log(item.amount);
                         const monthNo: number = parseInt(item.date.slice(5, 7));
                         expenses[monthNo - 1] += parseInt(item.amount);
@@ -79,15 +92,22 @@ const YearExpenseChart: React.FC = () => {
             localStorage.setItem('expenses', JSON.stringify(expenses));
         }
         setExpenseData([...expenses]);
-    }, [expenseList.list, year]);
+    }, [chartYear, expenseList.list, year]);
 
 
     return (
         <div className="chart-wrapper">
             <div className="chart" style={{ width: '600px', margin: 'auto', marginTop: '20px' }}>
+                <SelectBtn
+                    options={yearList}
+                    defaultVal={currentYear}
+                    val={chartYear}
+                    selectEventHandler={selectEventHandler}
+                    style={{ backgroundColor: 'white', width: '100px' }}
+                />
                 <Line options={options} data={data} />
             </div>
-            <h4>Expenses in 2023</h4>
+            <h4>Expenses in {chartYear}</h4>
         </div>
     )
 }
