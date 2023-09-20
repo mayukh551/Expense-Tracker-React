c4ac8e45a6dcb8cbdb563e47ff2db29f8fc9716e
mayukh551 <may14ukh@gmail.com>
2023-09-20 19:28:56 +0530

    done

--- src/Components/UI/WarningModal.tsx
+++ src/Components/UI/WarningModal.tsx
@@ -23,7 +23,7 @@ const WarningModal: React.FC<any> = ({ isOpen, onCancel, message, actionMessage,
                                 <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                     <h3 className="text-base font-semibold leading-6 text-gray-900" id="modal-title">Deactivate account</h3>
                                     <div className="mt-2">
-                                        <p className="text-sm text-gray-500">{message}</p>
+                                        <div className="text-sm font-semibold text-gray-500">{message}</div>
                                     </div>
                                 </div>
                             </div>
